0fe30a8ffd154db0e6c5bcadec08c10db7653707
Erik Humphrey <erik.humphrey@carleton.ca>
2019-12-04 19:07:25 -0500

    Progress on main() and change displayBoard()

--- a3/ascii_fill.py
+++ a3/ascii_fill.py
@@ -1,9 +1,16 @@
 # COMP 1405 Assignment 5
 # Name: John Appleseed
-# Student number: 101000000
+# Student number: 1010XXXXX
 
 def main():
-    displayBoard(readLevel(1))
+    board = readLevel(1)
+    displayBoard(board)
+    input = getUserAction(len(board), len(board[0]))
+    print(str(input[1]))
+    print(str(input[2]))
+    fill(board, board[int(input[1])][int(input[2])], input[0], input[1], input[2])
+
+
 
 def readLevel(num) -> list:
     with open("levels/ascii_fill_level" + str(num) + ".txt", "r") as file:
@@ -11,13 +18,29 @@ def readLevel(num) -> list:
         return(array)
 
 def displayBoard(lst):
-    print("   012345678", end="\n   ---------\n")
+    count = -1
+    print("   ", end="")
+    for char in lst[0]:
+        count = (count + 1) % 10
+        print(count, end="")
+    print("\n   ---------")
     count = 0
     for line in lst:
         count = count + 1
-        print("0" + str(count) + "|", end="")
-        for letter in line:
-            print(letter, end="")
+        print("{0:0=2d}".format(count) + "|", end="")
+        for char in line:
+            print(char, end="")
         print()
+
+def getUserAction(h, w) -> list:
+    symbol = input("Enter a symbol: ")
+    row = input("Select a row [0," + str(h) + "]: ")
+    col = input("Select a col [0," + str(w) + "]: ")
+    return [symbol, row, col]
+
+def fill(board, strT, strS, intR, intC):
+    print("lmao")
+
+
 
 main()
